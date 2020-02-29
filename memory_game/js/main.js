96ccf62faf216124d69abfc4b3b6e0a974a82ea9
Ryan Ho <ryanjho90@gmail.com>
2020-03-01 00:59:52 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,26 @@
 let cards = ["queen", "queen", "king", "king"];
 let cardsInPlay = [];
 
-let cardOne = cards[2];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-
-let cardTwo = cards[3];
-cardsInPlay.push(cardTwo);
-console.log("user flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[cardsInPlay.length - 1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again.");
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+		} else {
+		console.log("Sorry, try again.");
 	}
 }
+
+
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+}
+
+
+
+
+
+flipCard(0);
+flipCard(2);
