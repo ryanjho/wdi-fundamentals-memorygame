5db3b8eec216b2fedb638b191e7b120fa513981e
Ryan Ho <ryanjho90@gmail.com>
2020-02-29 15:37:49 +0800

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,8 @@
+let cardOne = "queen";
+let cardTwo = "queen";
+let cardThree = "king";
+let cardFour = "king";
+
+console.log("User flipped " + cardOne);
+
+console.log("user flipped " + cardThree);
