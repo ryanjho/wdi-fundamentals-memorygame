7d3ed899f9cb9b5ea00ca7c358762d361bd3886f
Ryan Ho <ryanjho90@gmail.com>
2020-03-01 22:04:26 +0800

    updated with javascript functionality (gameboard, flip card)

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -22,6 +22,20 @@ let cards = [
 ];
 let cardsInPlay = [];
 
+
+//Function that creates Game Board
+function createBoard() {
+	for (let i = 0; i < cards.length; i++) {
+		let cardElement = document.createElement("img");
+		cardElement.setAttribute("src", "images/back.png");
+		cardElement.setAttribute("data-id", i);
+		cardElement.addEventListener("click", flipCard);
+		document.getElementById("game-board").appendChild(cardElement);
+	}
+}
+
+
+// Checks for match between 2 flipped Cards
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
@@ -30,20 +44,22 @@ function checkForMatch() {
 	}
 }
 
+// Flips card
+function flipCard() {
+	let cardId = this.getAttribute("data-id");
+	this.setAttribute("src", cards[cardId].cardImage);
 
-function flipCard(cardId) {
+	console.log(cards[cardId]);
+
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 	cardsInPlay.push(cards[cardId].rank);
+
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	}
 }
 
 
-
-
-
-flipCard(0);
-flipCard(2);
+createBoard();
